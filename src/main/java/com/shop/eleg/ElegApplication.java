1331308c88dc370a4805c9f1442e7663e601e90a
VladislavKutsobin <v.kutsobin@gms-worldwide.com>
2022-10-03 20:37:22 +0300

    Initial commit

---
+++ src/main/java/com/shop/eleg/ElegApplication.java
@@ -0,0 +1,13 @@
+package com.shop.eleg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/eleg/ElegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.eleg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
